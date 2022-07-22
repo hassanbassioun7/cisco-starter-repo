a874dd9c9b51895e585afef807aea12fd5046bc1
hassanbassioun7 <97366708+hassanbassioun7@users.noreply.github.com>
2022-07-22 06:14:38 +0200

    Add files via upload

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+// import { Stack, Button } from 'react-bootstrap'
+import Container from 'react-bootstrap/esm/Container'
+import BudgetCard from './components/BudgetCard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar></Navbar>
+      <Container className='my-4'>
+      {/* <Stack direction='horizontal' gap="2" className='mb-4'>
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack> */}
+      <div 
+        style={{
+          display:"grid", 
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start"
+        }}
+      >
+        <BudgetCard 
+          name="Entertainment" 
+          gray
+          amount={200} 
+          max={1000}
+          inner="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        ></BudgetCard>
+        <BudgetCard 
+          name="Entertainment" 
+          gray
+          amount={1100} 
+          max={1000}
+          inner="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        ></BudgetCard>
+        <BudgetCard 
+          name="Entertainment" 
+          gray
+          amount={600} 
+          max={1000}
+          inner="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        ></BudgetCard>
+        <BudgetCard 
+          name="Entertainment" 
+          gray
+          amount={800} 
+          max={1000}
+          inner="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        ></BudgetCard>
+      </div>
+    </Container>
     </div>
-  );
+  )
 }
 
 export default App;
